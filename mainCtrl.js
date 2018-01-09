677892fc16103410799a17be77625cc5803babb2
Giao Nguyen <giao.nguyenn@yahoo.com>
2018-01-08 23:47:53 -0600

    completed 1/8

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,9 +1,81 @@
 module.exports = {
 
+	getUsers: (req, res, next) => {
+		req.app.get("db").get_users()
+		.then(response => res.json(response)
+    	.catch(err => console.log(err)))
+	},
 
+	getVehicles: (req, res, next) => {
+		req.app.get("db").get_vehicles()
+		.then(response => res.json(response)
+    	.catch(err => console.log(err)))
+	},
 
+	postUsers: (req, res, next) => {
+		const {name, email} = req.body;
 
+		req.app.get("db").post_users([name, email])
+		.then(response => res.json(response)
+    	.catch(err => console.log(err)))
+	},
 
+	postVehicles: (req, res, next) => {
+		const {make, model, year, owner_id} = req.body;
 
+		req.app.get("db").post_vehicles([make, model, year, owner_id])
+		.then(response => res.json(response)
+    	.catch(err => console.log(err)))
+	},
+
+	getVehicleCount: (req, res, next) => {
+		req.app.get("db").get_vehicle_count([req.params.userId])
+		.then(response => res.json(response)
+    	.catch(err => console.log(err)))
+	},
+
+	getVehiclesByUser: (req, res, next) => {
+		req.app.get("db").get_vehicles_by_user([req.params.userId])
+		.then(response => res.json(response)
+    	.catch(err => console.log(err)))
+	},
+
+	getVehiclesByEmail: (req, res, next) => {
+		const db = req.app.get("db");
+
+		if (req.query.userEmail) {
+			return db.get_vehicles_by_email(req.query.userEmail)
+			.then(response => res.json(response));
+		}
+		
+		if (req.query.userFirstStart) {
+			return db.get_vehicles_by_email_letters(req.query.userFirstStart + "%")
+			.then(response => res.json(response));
+		}
+	},
+
+	getVehiclesByYear: (req, res, next) => {
+		req.app.get("db").get_vehicles_by_year()
+		.then(response => res.json(response)
+    	.catch(err => console.log(err)))
+	},
+
+	changeVehicleOwner: (req, res, next) => {
+		req.app.get("db").change_vehicle_owner([req.params.vehicleId, req.params.userId])
+		.then(response => res.json(response)
+		.catch(err = console.log(err)))
+	},
+
+	deleteVehicleOwnership: (req, res, next) => {
+		req.app.get("db").delete_vehicle_ownership([req.params.vehicleId])
+		.then(response => res.json(response)
+		.catch(err => console.log(err)))
+	},
+
+	deleteVehicle: (req, res, next) => {
+		req.app.get("db").delete_vehicle([req.params.vehicleId])
+		.then(response => res.json(response)
+		.catch(err => console.log(err)))
+	}
 
 }
--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(cors());
 // You need to complete the information below to connect
 // to the assessbox database on your postgres server.
 massive({
-  host: //host,
-  port: //port,
-  database: //database,
-  user: //user,
-  password: //password
+  host: "localhost",//host,
+  port: 5432,//port,
+  database: "assessbox",//database,
+  user: "postgres",//user,
+  password: "postgres"//password
 }).then( db => {
   app.set('db', db);
 
@@ -33,11 +33,21 @@ massive({
 
 
 // ===== Build enpoints below ============
+app.get("/api/users", mainCtrl.getUsers);
+app.get("/api/vehicles", mainCtrl.getVehicles);
 
+app.post("/api/users", mainCtrl.postUsers);
+app.post("/api/vehicles", mainCtrl.postVehicles);
 
+app.get("/api/user/:userId/vehiclecount", mainCtrl.getVehicleCount);
+app.get("/api/user/:userId/vehicle", mainCtrl.getVehiclesByUser);
+app.get("/api/vehicle", mainCtrl.getVehiclesByEmail);
+app.get("/api/newervehiclesbyyear", mainCtrl.getVehiclesByYear);
 
+app.put("/api/vehicle/:vehicleId/user/:userId", mainCtrl.changeVehicleOwner);
 
-
+app.delete("/api/user/:userId/vehicle/:vehicleId", mainCtrl.deleteVehicleOwnership);
+app.delete("/api/vehicle/:vehicleId", mainCtrl.deleteVehicle);
 
 
 // ===== Do not change port ===============
